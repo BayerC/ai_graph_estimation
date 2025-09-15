020f8a703b3f06634896482a234f1425459a9701
Chris Bayer <c4.bayer@gmail.com>
2025-09-15 08:06:27 +0200

    deployment

--- src/deployment/app.py
+++ src/deployment/app.py
@@ -1,9 +1,25 @@
 import gradio as gr
+from fastai.vision.all import load_learner
+from pathlib import Path
+
+script_dir = Path(__file__).parent
+learner = load_learner(script_dir / "export.pkl")
 
 
-def greet(name):
-    return "Hello " + name + "!!"
+def predict(image):
+    pred_class, pred_idx, probs = learner.predict(image)
+    return pred_class
 
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+examples_dir = script_dir / "examples"
+demo = gr.Interface(
+    fn=predict,
+    inputs="image",
+    outputs="text",
+    examples=[
+        str(examples_dir / "linear.png"),
+        str(examples_dir / "quadratic.png"),
+        str(examples_dir / "cubic.png"),
+    ],
+)
 demo.launch()
